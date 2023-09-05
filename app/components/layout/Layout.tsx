8f7c339269a633957550072cd58a06c4595c8bb8
joannashin <joannashin3@gmail.com>
2023-09-05 20:14:38 +0900

    rem설정

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,9 +1,36 @@
 import Header from '@/components/header/Header';
 import style from './Layout.style';
+import { useEffect } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  function setFontSize() {
+    const htmlDoc = document.documentElement;
+
+    if (globalThis.innerWidth > globalThis.innerHeight) {
+      htmlDoc.style.fontSize = (globalThis.innerHeight / 320) * 62.5 + '%';
+    } else {
+      htmlDoc.style.fontSize = (globalThis.innerWidth / 320) * 62.5 + '%';
+    }
+  }
+
+  useEffect(() => {
+    let enSizing = false;
+
+    globalThis.dispatchEvent(new Event('resize'));
+
+    globalThis.onresize = function () {
+      if (!enSizing) {
+        globalThis.requestAnimationFrame(function () {
+          setFontSize();
+          enSizing = false;
+        });
+      }
+      enSizing = true;
+    };
+  }, []);
+
   return (
-    <div css={style} className="bgImage">
+    <div css={style}>
       <Header />
       <main>{children}</main>
     </div>
