184de1232a863dba26a8f607a6428ccb8495b7ca
joannashin <joannashin3@gmail.com>
2023-09-05 23:09:43 +0900

    rem세팅 수정

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -3,30 +3,35 @@ import style from './Layout.style';
 import { useEffect } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  function setFontSize() {
+  /**
+   * rem 세팅
+   */
+  const settingRem = () => {
     const htmlDoc = document.documentElement;
-
-    if (globalThis.innerWidth > globalThis.innerHeight) {
-      htmlDoc.style.fontSize = (globalThis.innerHeight / 320) * 62.5 + '%';
-    } else {
-      htmlDoc.style.fontSize = (globalThis.innerWidth / 320) * 62.5 + '%';
-    }
-  }
-
-  useEffect(() => {
     let enSizing = false;
 
-    globalThis.dispatchEvent(new Event('resize'));
+    function setFontSize() {
+      if (window.innerWidth > window.innerHeight) return;
 
-    globalThis.onresize = function () {
+      const remBaseFont = (htmlDoc.offsetWidth / 360) * 62.5; //10px 기준
+      htmlDoc.style.fontSize = `${remBaseFont > 62.5 ? remBaseFont : 62.5}%`;
+    }
+
+    window.onresize = function () {
       if (!enSizing) {
-        globalThis.requestAnimationFrame(function () {
+        window.requestAnimationFrame(function () {
           setFontSize();
           enSizing = false;
         });
       }
       enSizing = true;
     };
+
+    window.dispatchEvent(new Event('resize'));
+  };
+
+  useEffect(() => {
+    settingRem();
   }, []);
 
   return (
