4bc9fffd5b0310ff8998551ea3c20e861c837ccc
joannashin <joannashin3@gmail.com>
2023-10-04 02:35:08 +0900

    noticeSummary component 생성

---
+++ app/components/units/noticeSummary/NoticeSummary.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { YN } from '@/types/common';
+
+export interface Notice {
+  id: number;
+  type: string;
+  title: string;
+  detail: string;
+  readYn: YN;
+  date: string;
+}
+
+interface IProps {
+  noti: Notice;
+}
+
+const NoticeSummary = ({ noti }: IProps) => {
+  return (
+    <div>
+      <span>
+        <img src="/assets/images/icon/close.svg" alt={noti?.type} />
+      </span>
+      <div>
+        <p>{noti?.title}</p>
+      </div>
+      <div>
+        <p>{noti?.detail}</p>
+        {noti?.readYn && <span>안읽음</span>}
+      </div>
+    </div>
+  );
+};
+
+export default NoticeSummary;
