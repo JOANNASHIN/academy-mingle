4207677fb45fbc6c14319fb04ed92215d24d8a1a
joannashin <joannashin3@gmail.com>
2023-10-15 20:51:26 +0900

    공지사항 등록폼폼

--- app/components/modules/noticeAddForm/NoticeAddForm.tsx
+++ app/components/modules/noticeAddForm/NoticeAddForm.tsx
@@ -5,7 +5,23 @@ import React, { useState } from 'react';
 interface IProps {}
 
 const NoticeAddForm = () => {
-  return <>공지사항 등록 폼</>;
+  return (
+    <form action="">
+      <fieldset>
+        <legend>공지사항 등록 폼</legend>
+
+        <label>
+          <span>제목</span>
+          <input type="text" />
+        </label>
+
+        <label htmlFor="">
+          <span>내용</span>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </label>
+      </fieldset>
+    </form>
+  );
 };
 
 export default NoticeAddForm;
