54d51e226b8698dbdca0872a47056ba577fb0ddf
joannashin <joannashin3@gmail.com>
2023-11-12 23:01:27 +0900

    noticeForm style

--- app/components/modules/noticeAddForm/NoticeAddForm.style.tsx
+++ app/components/modules/noticeAddForm/NoticeAddForm.style.tsx
@@ -1,4 +1,53 @@
+import { color } from '@/styles/variants';
 import { css } from '@emotion/react';
 
-const style = css``;
+const style = css`
+  .form {
+    &__field {
+      padding: 0 1.2rem;
+    }
+    &__bottom {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 4rem;
+    }
+
+    &__submit {
+      padding: 1.2rem 2rem;
+      background: ${color.black};
+      color: ${color.white};
+      font-size: 1.4rem;
+      line-height: 1;
+    }
+  }
+
+  label {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-top: 2.4rem;
+
+    span {
+      flex: 0 0 4rem;
+      margin-right: 1rem;
+      font-size: 1.4rem;
+    }
+
+    input {
+      flex: 1 1 auto;
+      height: 3.5rem;
+      border: 1px solid ${color.greyddd};
+      border-radius: 1rem;
+    }
+
+    textarea {
+      flex: 0 0 100%;
+      height: 20rem;
+      margin-top: 1rem;
+      border-radius: 1rem;
+      border-color: ${color.greyddd};
+    }
+  }
+`;
 export default style;
--- app/components/modules/noticeAddForm/NoticeAddForm.tsx
+++ app/components/modules/noticeAddForm/NoticeAddForm.tsx
@@ -1,6 +1,7 @@
 import App from '@/constants/app';
 import classNames from 'classnames';
 import React, { FormEvent, useState } from 'react';
+import style from './NoticeAddForm.style';
 
 export interface Form {
   title: string;
@@ -31,22 +32,33 @@ const NoticeAddForm = ({ onSubmit }: IProps) => {
   };
 
   return (
-    <form onSubmit={e => handleSubmit(e)}>
-      <fieldset>
+    <form css={style} onSubmit={e => handleSubmit(e)}>
+      <fieldset className="form__field">
         <legend>공지사항 등록 폼</legend>
 
         <label>
           <span>제목</span>
-          <input type="text" value={form.title} onChange={e => handleUpdateForm('title', e.target.value)} />
+          <input
+            type="text"
+            value={form.title}
+            onChange={e => handleUpdateForm('title', e.target.value)}
+            maxLength={10}
+          />
         </label>
 
         <label>
           <span>내용</span>
-          <textarea value={form.content} onChange={e => handleUpdateForm('content', e.target.value)}></textarea>
+          <textarea
+            value={form.content}
+            onChange={e => handleUpdateForm('content', e.target.value)}
+            maxLength={50}
+          ></textarea>
         </label>
       </fieldset>
-      <div>
-        <button type="submit">submit</button>
+      <div className="form__bottom">
+        <button type="submit" className="form__submit">
+          등록
+        </button>
       </div>
     </form>
   );
